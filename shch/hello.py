47ea3f272777d1359d5b7a9030a4bd50afea27d2
shch91 <github2016>
2018-10-11 21:28:20 +0800

    格式

--- shch/hello.py
+++ shch/hello.py
@@ -1,15 +1,3 @@
-import redis
-import sys
-import time
 
-r=redis.StrictRedis(host='127.0.0.1',port=6379,db=0)
-try:
-    id=sys.argv[1]
-except:
-    print('input argument error')
-    sys.exit(0)
-if r.llen(id)>=5 and time.time()-float(r.lindex(id,4))<=3600:
-    print("you are forbidden logining" )
-else:
-    print("you are allow to login")
-    r.lpush(id,time.time())
+
+print("fdsfa范德萨")
